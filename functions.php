8cbd60d85ff242dc4f001bde3951f6ff28a92e4b
WanneBeJames <jamesnjo@gmail.com>
2018-09-01 08:41:28 -0700

    edited footer, functions, and header to implement menu functions

--- functions.php
+++ functions.php
@@ -6,3 +6,13 @@ function wannebe_script_enqueue() {
 }
 
 add_action('wp_enqueue_scripts', 'wannebe_script_enqueue');
+
+function wannebe_theme_setup() {
+
+    add_theme_support('menus');
+
+    register_nav_menu('primary', 'Primary Header Navigation');
+    register_nav_menu('secondary', 'Footer Navigation');
+}
+
+add_action('init', 'wannebe_theme_setup');
