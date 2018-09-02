2a311073dd43705bb8c6c6be479843c96d6bf9b4
WanneBeJames <jamesnjo@gmail.com>
2018-09-02 15:50:12 -0700

    made edits to the index and header files in order to implement post loop and body class functionality

--- header.php
+++ header.php
@@ -2,10 +2,21 @@
 <html>
         <head>
             <meta charset="utf-8">
-            <title>Wannebe Theme</title>
+            <title>SteeperThanDeep</title>
             <?php wp_head(); ?>
 </head>
 
-<body>
+<?php
+
+    if( is_front_page() ):
+        $wannebe_classes = array('wannebe-class', 'my-class');
+    else:
+        $wannebe_classes = array('no-wannebe-class');
+    endif;
+
+?>
+
+
+<body <?php body_class( $wannebe_classes ); ?>>
 
     <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
