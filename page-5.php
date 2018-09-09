7849a98fb86dfa6385ee68fc054a47fc6230897d
WanneBeJames <jamesnjo@gmail.com>
2018-09-09 11:49:08 -0700

    Correction in header and addition of a sidebar

--- page-5.php
+++ page-5.php
@@ -1,21 +1,33 @@
 <?php get_header(); ?>
 
-    <?php
-
-    if ( have_posts() ):
-
-        while ( have_posts() ): the_post(); ?>
-
-            <h3><?php the_title(); ?></h3>
-
-            <p><?php the_content(); ?></p>
-
-            <hr>
-
-        <?php endwhile;
-
-    endif;
-
-    ?>
+<div class="row">
+	
+	<div class="col-xs-12 col-sm-8">
+		
+		<?php 
+		
+		if( have_posts() ):
+			
+			while( have_posts() ): the_post(); ?>
+				
+				<p><?php the_content(); ?></p>
+				
+				<h3><?php the_title(); ?></h3>
+				
+				<hr>
+			
+			<?php endwhile;
+			
+		endif;
+				
+		?>
+	
+	</div>
+	
+	<div class="col-xs-12 col-sm-4">
+		<?php get_sidebar(); ?>
+	</div>
+	
+</div>
 
 <?php get_footer(); ?>
