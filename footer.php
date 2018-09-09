6b888a4de3fc17ab9b4c4240712c3489697d9939
WanneBeJames <jamesnjo@gmail.com>
2018-09-09 10:32:34 -0700

    cleanup and reversion to V3.3.4 of bootstrap

--- footer.php
+++ footer.php
@@ -3,8 +3,7 @@
             <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
         </footer>
 
-    </div>  
-    
+    </div><!-- .container -->    
 
     <?php wp_footer(); ?>
 
--- functions.php
+++ functions.php
@@ -6,12 +6,12 @@
 */
 function wannebe_script_enqueue() {
     //CSS
-    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(),  '4.1.3', 'all');
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(),  '3.3.4', 'all');
     wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/wannebe.css', array(),  '1.0.0', 'all');
     
     //js
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '4.1.3', true);
+    wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.4', true);
     wp_enqueue_script('customjs', get_template_directory_uri() . '/js/wannebe.js', array(), '1.0.0', true);
     
 }
