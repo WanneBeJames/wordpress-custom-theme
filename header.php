3dc9c5bf5f179545a16e44a4c32785001a75d7bc
WanneBeJames <jamesnjo@gmail.com>
2018-09-17 08:27:57 -0700

    mass revision 3 and implementation of post edit and post tags -- plus html5 search bar

--- header.php
+++ header.php
@@ -28,9 +28,7 @@
 					  <div class="container-fluid">
 					    <!-- Brand and toggle get grouped for better mobile display -->
 					    <div class="navbar-header">
-
-                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 					      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					        <span class="sr-only">Toggle navigation</span>
 					        <span class="icon-bar"></span>
@@ -52,6 +50,10 @@
 					  </div><!-- /.container-fluid -->
 					</nav>
 				
+				</div>
+				
+				<div class="search-form-container">
+					<?php get_search_form(); ?>
 				</div>
 				
 			</div>
